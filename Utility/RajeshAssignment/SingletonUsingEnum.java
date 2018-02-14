c80c031af4db1133ee2f390cfeaffe3b7ea06c56
Divya Gupta <Divya_Gupta@epam.com>
2018-02-14 11:30:41 +0530

    RajeshAssignmentcommit14

--- Utility/RajeshAssignment/SingletonUsingEnum.java
+++ Utility/RajeshAssignment/SingletonUsingEnum.java
@@ -2,13 +2,27 @@ package RajeshAssignment;
 
 public class SingletonUsingEnum {
 	
-	
-	
-	
-	
+	public static void main(String a[])
+	{
 
+		SingleTonDemo obj1 = SingleTonDemo.INSTANCE;
+		System.out.println(obj1);
+		obj1.i= 5;
+		obj1.show();
+		
+		SingleTonDemo obj2 = SingleTonDemo.INSTANCE;
+		obj1.i= 6;
+		obj1.show();
+	}
 }
-/*enum{
+enum SingleTonDemo{
+	
+	INSTANCE;
+	int i;
+	public void show()
+	{
+		System.out.println(i);
+	}
+	
 	
 }
-*/
